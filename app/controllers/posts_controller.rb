faf8b97447edf8e8cafdac946a7f761439e8f1c7
tontonDaiDon <187248409+tontonDaiDon@users.noreply.github.com>
2024-11-25 16:26:15 +0900

    [add]画像投稿機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,7 +36,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:location, :text)
+    params.require(:post).permit(:location, :text, :image)
   end
 
 end

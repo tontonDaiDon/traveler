63212d2a4fcd8c4be96bc255d79bc4fbfff2008c
tontonDaiDon <187248409+tontonDaiDon@users.noreply.github.com>
2025-01-03 10:10:29 +0900

    [update]いいね機能完成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,13 @@
 class FavoritesController < ApplicationController
     def create
         post= Post.find(params[:post_id])
-        favorite = curent_user.favorites.new(post.id: post.id)
+        favorite = current_user.favorites.new(post_id: post.id)
         favorite.save
         redirect_to post_path(post)
     end
 
     def destroy
-        post = Post.find(paramas[:post_id])
+        post = Post.find(params[:post_id])
         favorite = current_user.favorites.find_by(post_id: post.id)
         favorite.destroy
         redirect_to post_path(post)      
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,6 @@ class Post < ApplicationRecord
     has_many :favorites, dependent: :destroy
 
     def favorited_by?(user)
-        favorites.where(user_id: user.id).exsists?
+        favorites.where(user_id: user.id).exists?
     end
 end

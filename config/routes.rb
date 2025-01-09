36d45fe3985ca04b59d361f50bc281ab09a10395
tontonDaiDon <187248409+tontonDaiDon@users.noreply.github.com>
2025-01-09 18:12:45 +0900

    [update]マイページ編集追加，レイアウト整備要

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only:[:index, :show]
+  resources :users, only:[:index, :show, :edit, :update]
   resources :posts do
     resources :comments, only:[:create, :destroy]
     resource :favorites, only:[:create, :destroy]
---
+++ db/migrate/20250109050532_removeprofile_from_users.rb
@@ -0,0 +1,4 @@
+class RemoveprofileFromUsers < ActiveRecord::Migration[7.1]
+  def change
+  end
+end

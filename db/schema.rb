6cf858f0809658cee23b64f272a5235645a4ec2c
tontonDaiDon <187248409+tontonDaiDon@users.noreply.github.com>
2025-01-12 20:00:03 +0900

    [update]フォローフォロワー機能モデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_09_050737) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_09_171338) do
   create_table "comments", force: :cascade do |t|
     t.text "comment"
     t.integer "user_id"
@@ -35,6 +35,15 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_09_050737) do
     t.integer "user_id"
   end
 
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id", null: false
+    t.integer "followed_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -50,4 +59,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_09_050737) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "relationships", "users", column: "followed_id"
+  add_foreign_key "relationships", "users", column: "follower_id"
 end

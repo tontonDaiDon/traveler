e88a765707068fe1cbcb49623bea5056199de845
tontonDaiDon <187248409+tontonDaiDon@users.noreply.github.com>
2025-01-13 23:52:00 +0900

    [update]モデル作成外部キー指定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
 
   attachment :profile_image
 end

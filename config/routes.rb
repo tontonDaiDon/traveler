19c7b5ce78e81e818bc9dcc2e50506dae1e62147
tontonDaiDon <187248409+tontonDaiDon@users.noreply.github.com>
2025-01-16 23:52:02 +0900

    [update]フォローフォロワー機能ビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :posts do
+    resources :comments, only:[:create, :destroy]
+    resource :favorites, only:[:create, :destroy]
+end
   resources :users, only:[:index, :show, :edit, :update] do
     member do
       get :follows, :followers
